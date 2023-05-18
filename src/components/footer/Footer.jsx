435dd68d99ef2dea9bf7e80f79a0df091ef63da2
Espen Iversen <espeiv@stud.ntnu.no>
2023-05-18 23:46:40 +0200

    small change to footer reference

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,8 @@ const Footer = () => {
     <footer className="footer">
       <p className="footer-text">
         &copy; Copyright 2023, Espen Iversen,{" "}
-        <a
-          href="https://www.flaticon.com/free-icons/gradient"
-          title="gradient icons"
-        >
-          Gradient icons created by jeremie ROBERRINI-NEVEU - Flaticon
+        <a href="https://www.flaticon.com/free-icons/" title="gradient icons">
+          Icons created by jeremie robberini-neveu - flaticon
         </a>
       </p>
     </footer>
